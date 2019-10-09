b248af04397cfb120155d3098c25098828025aef
wowiczek@gmail.com <chudychudy01>
2019-10-09 12:57:35 +0200

    test framework change

---
+++ util/file_util_integration_test.go
@@ -0,0 +1,88 @@
+package util
+
+import (
+	proto "dominikw.pl/wnc_plugin/proto"
+	"fmt"
+	"github.com/google/logger"
+	"github.com/stretchr/testify/suite"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+)
+
+type IntegrationFileOperationsTests struct {
+	suite.Suite
+	correctFilenameForMS  string
+	correctFilenameForBMS string
+}
+
+func (s *IntegrationFileOperationsTests) BeforeTest(_, testName string) {
+	now := time.Now()
+	if !strings.Contains(testName, "Today") {
+		now = time.Date(2000, 1, 1, 1, 1, 1, 1, time.UTC)
+	}
+	s.correctFilenameForMS = fmt.Sprintf("MethodServer1-%s%s%s319-30967-log4j.log",
+		strconv.Itoa(now.Year())[:2],
+		fmt.Sprintf("%02d", now.Month()),
+		fmt.Sprintf("%02d", now.Day()))
+	s.correctFilenameForBMS = fmt.Sprintf("BackgroundMethodServer1-%s%s%s319-30967-log4j.log",
+		strconv.Itoa(now.Year())[:2],
+		fmt.Sprintf("%02d", now.Month()),
+		fmt.Sprintf("%02d", now.Day()))
+	createFiles(s)
+}
+
+func (s *IntegrationFileOperationsTests) AfterTest(_, b string) {
+	removeFiles()
+}
+
+func TestFindLogFile(t *testing.T) {
+	suite.Run(t, new(IntegrationFileOperationsTests))
+}
+
+func (suite *IntegrationFileOperationsTests) TestShouldFindBmsStartedToday() {
+	msLocation := proto.LogFileLocation{FileLocation: "test", LogType: proto.LogFileLocation_BACKGROUND_METHOD_SERVER}
+	s, _ := FindLogFile(&msLocation)
+	suite.Equal(suite.correctFilenameForBMS, s, "should be equal")
+}
+
+func (suite *IntegrationFileOperationsTests) TestShouldFindMsStartedToday() {
+	msLocation := proto.LogFileLocation{FileLocation: "test", LogType: proto.LogFileLocation_METHOD_SERVER}
+	s, _ := FindLogFile(&msLocation)
+	suite.Equal(suite.correctFilenameForMS, s, "should be equal")
+}
+
+func (suite *IntegrationFileOperationsTests) TestShouldFindBmsStartedBefore() {
+	msLocation := proto.LogFileLocation{FileLocation: "test", LogType: proto.LogFileLocation_BACKGROUND_METHOD_SERVER}
+	s, _ := FindLogFile(&msLocation)
+	suite.Equal(suite.correctFilenameForBMS, s, "should be equal")
+}
+
+func (suite *IntegrationFileOperationsTests) TestShouldFindMsStartedBefore() {
+	msLocation := proto.LogFileLocation{FileLocation: "test", LogType: proto.LogFileLocation_METHOD_SERVER}
+	s, _ := FindLogFile(&msLocation)
+	suite.Equal(suite.correctFilenameForMS, s, "should be equal")
+}
+
+func removeFiles() {
+	e := os.RemoveAll("test")
+	if e != nil {
+		logger.Fatal(e)
+	}
+}
+
+func createFiles(s *IntegrationFileOperationsTests) {
+	_ = os.Mkdir("test", 777)
+	createFile("test/" + s.correctFilenameForMS)
+	createFile("test/" + s.correctFilenameForBMS)
+}
+
+func createFile(name string) {
+	f, e := os.OpenFile(name, os.O_CREATE, 777)
+	if e != nil {
+		logger.Fatal(e)
+	}
+	defer f.Close()
+}
---
+++ util/file_util_unit_test.go
@@ -0,0 +1,41 @@
+package util
+
+import (
+	proto "dominikw.pl/wnc_plugin/proto"
+	"fmt"
+	"github.com/stretchr/testify/suite"
+	"strconv"
+	"testing"
+	"time"
+)
+
+type UnitFileOperationsTests struct {
+	suite.Suite
+	correctFilenameForMS  string
+	correctFilenameForBMS string
+}
+
+func TestCheckFileName(t *testing.T) {
+	suite.Run(t, new(UnitFileOperationsTests))
+}
+
+func (s *UnitFileOperationsTests) BeforeTest(_, _ string) {
+	now := time.Now()
+	s.correctFilenameForMS = fmt.Sprintf("MethodServer1-%s%s%s319-30967-log4j.log",
+		strconv.Itoa(now.Year())[:2],
+		fmt.Sprintf("%02d", now.Month()),
+		fmt.Sprintf("%02d", now.Day()))
+	s.correctFilenameForBMS = fmt.Sprintf("BackgroundMethodServer1-%s%s%s319-30967-log4j.log",
+		strconv.Itoa(now.Year())[:2],
+		fmt.Sprintf("%02d", now.Month()),
+		fmt.Sprintf("%02d", now.Day()))
+}
+
+func (s *UnitFileOperationsTests) TestCheckFileNameShouldWork() {
+	s.True(checkFileName(s.correctFilenameForMS, proto.LogFileLocation_METHOD_SERVER), "%s Should be correct!", s.correctFilenameForMS)
+}
+
+func (s *UnitFileOperationsTests) TestCheckFileNameShouldFail() {
+	name := "random test string"
+	s.False(checkFileName(name, proto.LogFileLocation_METHOD_SERVER), "%s Should be incorrect!", name)
+}
