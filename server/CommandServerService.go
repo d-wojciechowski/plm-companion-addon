ac755057e55ca84849b0c4fb126a9f2daba8248a
Dominik Wojciechowski <doominikwoojciechowski@gmail.com>
2020-02-25 20:02:07 +0100

    changed error handling under execution

--- server/CommandServerService.go
+++ server/CommandServerService.go
@@ -6,15 +6,16 @@ import (
 	"dominikw.pl/wnc_plugin/proto/commands"
 	"github.com/golang/protobuf/proto"
 	"github.com/google/logger"
-	"github.com/pkg/errors"
 	"github.com/rsocket/rsocket-go/payload"
 	"github.com/rsocket/rsocket-go/rx"
 	"github.com/rsocket/rsocket-go/rx/flux"
 	"github.com/rsocket/rsocket-go/rx/mono"
+	"io"
 	"os"
 	"os/exec"
 	"runtime"
 	"strconv"
+	"sync"
 	"time"
 )
 
@@ -26,9 +27,6 @@ func (srv *Server) Execute(msg payload.Payload) mono.Mono {
 		return mono.Just(toPayload(&commands.Response{Message: "NO WNC MODE", Status: commands.Status_FINISHED}, make([]byte, 1)))
 	}
 
-	if command.GetArgs() != "" {
-		command.Args = ""
-	}
 	cmd := execCommand(command)
 	out, _ := cmd.CombinedOutput()
 	return mono.Just(toPayload(&commands.Response{Message: string(out), Status: commands.Status_FINISHED}, make([]byte, 1)))
@@ -50,30 +48,26 @@ func (srv *Server) ExecuteStreaming(msg payload.Payload) flux.Flux {
 
 	var cmd *exec.Cmd
 	return flux.Create(func(ctx context.Context, s flux.Sink) {
-		if command.GetArgs() != "" {
-			command.Args = ""
-		}
 		cmd = execCommand(command)
 
 		stdout, err := cmd.StdoutPipe()
 		errorPiper, err := cmd.StderrPipe()
 
+		var wg sync.WaitGroup
+		wg.Add(2)
 		go func() {
-			scanner := bufio.NewScanner(stdout)
-			scanner.Split(bufio.ScanLines)
-			for scanner.Scan() {
-				m := scanner.Text()
-				s.Next(toPayload(&commands.Response{Message: m, Status: commands.Status_RUNNING}, make([]byte, 1)))
-			}
-			s.Complete()
+			pipeReader(stdout, s)
+			wg.Done()
 		}()
 
 		go func() {
-			scanner := bufio.NewScanner(errorPiper)
-			scanner.Split(bufio.ScanLines)
-			for scanner.Scan() {
-				s.Error(errors.New(scanner.Text()))
-			}
+			pipeReader(errorPiper, s)
+			wg.Done()
+		}()
+
+		go func() {
+			wg.Wait()
+			s.Complete()
 		}()
 
 		_ = cmd.Start()
@@ -84,6 +78,15 @@ func (srv *Server) ExecuteStreaming(msg payload.Payload) flux.Flux {
 	}).DoFinally(func(s rx.SignalType) {
 		kill(cmd)
 	})
+}
+
+func pipeReader(errorPiper io.ReadCloser, s flux.Sink) {
+	scanner := bufio.NewScanner(errorPiper)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		m := scanner.Text()
+		s.Next(toPayload(&commands.Response{Message: m, Status: commands.Status_RUNNING}, make([]byte, 1)))
+	}
 }
 
 func kill(cmd *exec.Cmd) {
