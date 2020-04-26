e6bd079e0a0ceeabd7bdaca029a4f0e616fe031a
Dominik Wojciechowski <doominikwoojciechowski@gmail.com>
2020-04-26 12:25:40 +0200

    updated build scripts

--- make.ps1
+++ make.ps1
@@ -1,6 +1,9 @@
 protoc --proto_path=proto --go_out=plugins=grpc:proto commands/commands.proto
 protoc --proto_path=proto --go_out=plugins=grpc:proto files/files.proto
 
+$env:PRODUCT_NAME="PLMCompanionAddon"
+Write-Output("Building $env:PRODUCT_NAME`n")
+
 Write-Output("Current GOOS $env:GOOS : Current GOARCH :$env:GOARCH.`n")
 $env:OLDGOOS=$env:GOOS
 $env:OLDGOARCH=$env:GOARCH
@@ -8,25 +11,25 @@ $env:OLDGOARCH=$env:GOARCH
 $env:GOARCH="amd64"
 $env:GOOS="linux"
 Write-Output("Linux x64 build| GOOS $env:GOOS : GOARCH :$env:GOARCH.")
-go build -ldflags="-w -s" -o distr/WncPlugin-linux-x64
+go build -ldflags="-w -s" -o distr/$env:PRODUCT_NAME-linux-x64
 Write-Output("Linux build done.`n")
 
 $env:GOARCH="386"
 $env:GOOS="linux"
 Write-Output("Linux x86 build| GOOS $env:GOOS : GOARCH :$env:GOARCH.")
-go build -o distr/WncPlugin-linux-x86
+go build -o distr/$env:PRODUCT_NAME-linux-x86
 Write-Output("Linux x86 build done.`n")
 
 $env:GOARCH="amd64"
 $env:GOOS="windows"
 Write-Output("Windows x64 build| GOOS $env:GOOS : GOARCH :$env:GOARCH.")
-go build -o distr/WncPlugin-windows-x64.exe
+go build -o distr/$env:PRODUCT_NAME-windows-x64.exe
 Write-Output("Windows x64 build done.`n")
 
 $env:GOARCH="amd64"
 $env:GOOS="darwin"
 Write-Output("MacOS x64 build| GOOS $env:GOOS : GOARCH :$env:GOARCH.")
-go build -o distr/WncPlugin-macos-x64
+go build -o distr/$env:PRODUCT_NAME-macos-x64
 Write-Output("MacOS x64 build done.")
 
 $env:GOOS=$env:OLDGOOS
--- make.sh
+++ make.sh
@@ -1,21 +1,34 @@
 protoc --proto_path=proto --go_out=plugins=grpc:proto commands/commands.proto
 protoc --proto_path=proto --go_out=plugins=grpc:proto files/files.proto
 
+export PRODUCT_NAME="PLMCompanionAddon"
+echo "Building $PRODUCT_NAME"
+
+echo "Current GOOS $GOOS : Current GOARCH $GOARCH"
+
 export GOARCH=amd64
 export GOOS=linux
-go build -o distr/WncPlugin-linux-x64
+echo "Linux x64 build| GOOS $GOOS : GOARCH :$GOARCH."
+go build -o distr/$PRODUCT_NAME-linux-x64
+echo "Linux build done."
 
 export GOARCH=386
 export GOOS=linux
-go build -o distr/WncPlugin-linux-x86
+echo "Linux x86 build| GOOS $GOOS : GOARCH :$GOARCH."
+go build -o distr/$PRODUCT_NAME-linux-x86
+echo "Linux x86 build done."
 
 export GOARCH=amd64
 export GOOS=windows
-go build -o distr/WncPlugin-windows-x64.exe
+echo "Windows x64 build| GOOS $GOOS : GOARCH :$GOARCH."
+go build -o distr/$PRODUCT_NAME-windows-x64.exe
+echo "Windows x64 build done."
 
 export GOARCH=amd64
 export GOOS=darwin
-go build -o distr/WncPlugin-macos-x64
+echo "MacOS x64 build| GOOS $GOOS : GOARCH :$GOARCH."
+go build -o distr/$PRODUCT_NAME-macos-x64
+echo "MacOS x64 build done."
 
 export GOARCH=$OLDGOARCH
 export GOOS=$OLDGOOS
